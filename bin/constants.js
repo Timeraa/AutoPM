1b6cb83a7fd79aad6c1f85b270aee893fa35410c
Bas950 <34792838+Bas950@users.noreply.github.com>
2021-01-15 03:00:17 +0100

    :bug: Whoopsy

--- bin/constants.js
+++ bin/constants.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.REQUIREREGEX = exports.IMPORTREGEX = exports.MODULENAMEREGEX = void 0;
-exports.MODULENAMEREGEX = "(@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~/]*", exports.IMPORTREGEX = new RegExp("import(\\s([^\"'`]*\\sfrom\\s)?([\"'`])(" + exports.MODULENAMEREGEX + ")\\2|\\(([\"'`])(" + exports.MODULENAMEREGEX + ")\\4\\));?", "g"), exports.REQUIREREGEX = new RegExp("require\\(([\"'`])(" + exports.MODULENAMEREGEX + ")\\1\\)", "g");
+exports.MODULENAMEREGEX = "(@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~/]*", exports.IMPORTREGEX = new RegExp("import(\\s([^\"'`]*\\sfrom\\s)?([\"'`])(" + exports.MODULENAMEREGEX + ")\\3|\\(([\"'`])(" + exports.MODULENAMEREGEX + ")\\5\\));?", "g"), exports.REQUIREREGEX = new RegExp("require\\(([\"'`])(" + exports.MODULENAMEREGEX + ")\\1\\)", "g");
 //# sourceMappingURL=constants.js.map
--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const MODULENAMEREGEX =
 		"(@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~/]*",
 	IMPORTREGEX = new RegExp(
-		`import(\\s([^"'\`]*\\sfrom\\s)?(["'\`])(${MODULENAMEREGEX})\\2|\\((["'\`])(${MODULENAMEREGEX})\\4\\));?`,
+		`import(\\s([^"'\`]*\\sfrom\\s)?(["'\`])(${MODULENAMEREGEX})\\3|\\((["'\`])(${MODULENAMEREGEX})\\5\\));?`,
 		"g"
 	),
 	REQUIREREGEX = new RegExp(
